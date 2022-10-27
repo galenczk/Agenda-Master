4eca1ca94f3a7a576e7dcc7d5b089f33272a6cf9
galenczk <ciszekg@oregonstate.edu>
2022-10-27 12:21:13 -0700

    Changes to page titles.  Fixed mistakes on page components.

--- src/pages/CertPage.js
+++ src/pages/CertPage.js
@@ -1,20 +1,101 @@
 // Import dependencies
 import React from "react";
 import { useFormik } from "formik";
-import { useNavigate } from "react-router-dom";
 
 // Import components
 
 // Page function
-export default function DeveloperPage() {
-  // Page navigation
-  const navigate = useNavigate();
+export default function CertPage() {
+  const formik = useFormik({
+    initialValues: {
+      projectTitle: "",
+      description: "",
+      deliveryDate: "",
+      projectStatus: "",
+      customerID: "",
+      projectID: "",
+    },
+  });
+
+  function resetForm() {
+    formik.resetForm();
+  }
 
   // DOM return
   return (
     <>
-      <h3 class="text-3xl">Projects</h3>
+      <h3 class="text-3xl p-6">Certifications</h3>
+      <div id="Search Display Table" class="place-self-center">
+        <table class="border-4 mb-6">
+          <thead>
+            <tr>
+              <th class="p-2 bg-slate-200">Project ID</th>
+              <th class="p-2 bg-slate-200">Title</th>
+              <th class="p-2 bg-slate-200">Description</th>
+              <th class="p-2 bg-slate-200">Delivery Date</th>
+              <th class="p-2 bg-slate-200">Status</th>
+              <th class="p-2 bg-slate-200">Customer</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td class="p-1 border-2 text-center">1</td>
+              <td class="p-1 border-2">Excavation Log</td>
+              <td class="p-1 border-2">Archaeological mapping service</td>
+              <td class="p-1 border-2 text-center">2023-10-02</td>
+              <td class="p-1 border-2">In Progress</td>
+              <td class="p-1 border-2 text-center">4</td>
+            </tr>
+            <tr>
+              <td class="p-1 border-2 text-center">2</td>
+              <td class="p-1 border-2">Fingerprint Recognition Banking</td>
+              <td class="p-1 border-2">
+                Fingerprint account access restriction
+              </td>
+              <td class="p-1 border-2 text-center">2024-08-01</td>
+              <td class="p-1 border-2">In Progress</td>
+              <td class="p-1 border-2 text-center">2</td>
+            </tr>
+            <tr>
+              <td class="p-1 border-2 text-center">3</td>
+              <td class="p-1 border-2">Grocery Budget Tracker</td>
+              <td class="p-1 border-2">Grocery shopping budget app</td>
+              <td class="p-1 border-2 text-center">2022-12-09</td>
+              <td class="p-1 border-2">Completed</td>
+              <td class="p-1 border-2 text-center">5</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
 
+      <div id="Search Form" class="p-8 bg-gray-300">
+        <form
+          class="flex flex-col"
+          onSubmit={(event) => {
+            event.preventDefault();
+            resetForm();
+          }}
+        >
+          <legend class="text-lg font-bold text-center">
+            Search Projects
+          </legend>
+
+          <label for="title">Project Title</label>
+          <input
+            type="text"
+            id="title"
+            name="title"
+            placeholder="Search for Projects"
+            onChance={formik.handleChange}
+            value={formik.values.projectTitle}
+          />
+
+          <div class="flex justify-between mt-6">
+            <button class="btn btn-blue">Search</button>
+            <button class="btn btn-gray">Reset</button>
+          </div>
+        </form>
+      </div>
       <div class="h-16" />
 
       <div class="p-8 bg-gray-300">
--- src/pages/ProjectPage.js
+++ src/pages/ProjectPage.js
@@ -1,45 +1,98 @@
 // Import dependencies
 import React from "react";
 import { useFormik } from "formik";
-import { useNavigate } from "react-router-dom";
 
 // Import components
 
 // Page function
-export default function DeveloperPage() {
-  
-  // Page navigation
-  const navigate = useNavigate();
+export default function ProjectPage() {
+  const formik = useFormik({
+    initialValues: {
+      projectTitle: "",
+      description: "",
+      deliveryDate: "",
+      projectStatus: "",
+      customerID: "",
+      projectID: "",
+    },
+  });
+
+  function resetForm() {
+    formik.resetForm();
+  }
 
   // DOM return
   return (
     <>
-      <h3 class="text-3xl">Projects</h3>
+      <h3 class="text-3xl p-6">Projects</h3>
 
-      <div class="h-16" />
+      <div id="Search Display Table" class="place-self-center">
+        <table class="border-4 mb-6">
+          <thead>
+            <tr>
+              <th class="p-2 bg-slate-200">Project ID</th>
+              <th class="p-2 bg-slate-200">Title</th>
+              <th class="p-2 bg-slate-200">Description</th>
+              <th class="p-2 bg-slate-200">Delivery Date</th>
+              <th class="p-2 bg-slate-200">Status</th>
+              <th class="p-2 bg-slate-200">Customer</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td class="p-1 border-2 text-center">1</td>
+              <td class="p-1 border-2">Excavation Log</td>
+              <td class="p-1 border-2">Archaeological mapping service</td>
+              <td class="p-1 border-2 text-center">2023-10-02</td>
+              <td class="p-1 border-2">In Progress</td>
+              <td class="p-1 border-2 text-center">4</td>
+            </tr>
+            <tr>
+              <td class="p-1 border-2 text-center">2</td>
+              <td class="p-1 border-2">Fingerprint Recognition Banking</td>
+              <td class="p-1 border-2">
+                Fingerprint account access restriction
+              </td>
+              <td class="p-1 border-2 text-center">2024-08-01</td>
+              <td class="p-1 border-2">In Progress</td>
+              <td class="p-1 border-2 text-center">2</td>
+            </tr>
+            <tr>
+              <td class="p-1 border-2 text-center">3</td>
+              <td class="p-1 border-2">Grocery Budget Tracker</td>
+              <td class="p-1 border-2">Grocery shopping budget app</td>
+              <td class="p-1 border-2 text-center">2022-12-09</td>
+              <td class="p-1 border-2">Completed</td>
+              <td class="p-1 border-2 text-center">5</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
 
-      <div class="p-8 bg-gray-300">
+      <div id="Search Form" class="p-8 bg-gray-300">
         <form
           class="flex flex-col"
           onSubmit={(event) => {
             event.preventDefault();
+            resetForm();
           }}
         >
-          <legend class="text-lg font-bold text-center">Add a Project</legend>
+          <legend class="text-lg font-bold text-center">
+            Search Projects
+          </legend>
 
           <label for="title">Project Title</label>
-          <input type="text" id="title" name="title" />
-          <label for="description">Description</label>
-          <input type="text" id="description" name="description" />
-          <label for="delivery_date">Delivery Date</label>
-          <input type="date" id="delivery_date" name="delivery_date" />
-          <label for="proj_status">Project Status</label>
-          <input type="text" id="proj_status" name="proj_status" />
-          <label for="customer_id">Customer ID</label>
-          <input type="text" id="customer_id" name="customer_id" />
+          <input
+            type="text"
+            id="title"
+            name="title"
+            placeholder="Search for Projects"
+            onChance={formik.handleChange}
+            value={formik.values.projectTitle}
+          />
 
           <div class="flex justify-between mt-6">
-            <button class="btn btn-green">Add</button>
+            <button class="btn btn-blue">Search</button>
             <button class="btn btn-gray">Reset</button>
           </div>
         </form>
@@ -47,7 +100,69 @@ export default function DeveloperPage() {
 
       <div class="h-16" />
 
-      <div class="p-8 bg-gray-300">
+      <div id="Add Project Form" class="p-8 bg-gray-300">
+        <form
+          class="flex flex-col"
+          onSubmit={(event) => {
+            event.preventDefault();
+            resetForm();
+          }}
+        >
+          <legend class="text-lg font-bold text-center">Add a Project</legend>
+
+          <label for="projectTitle">Project Title</label>
+          <input
+            type="text"
+            id="projectTitle"
+            name="projectTitle"
+            onChange={formik.handleChange}
+            value={formik.values.projectTitle}
+          />
+          <label for="description">Description</label>
+          <input
+            type="text"
+            id="description"
+            name="description"
+            onChange={formik.handleChange}
+            value={formik.values.description}
+          />
+          <label for="delivery_date">Delivery Date</label>
+          <input
+            type="date"
+            id="deliveryDate"
+            name="deliveryDate"
+            onChange={formik.handleChange}
+            value={formik.values.deliveryDate}
+          />
+          <label for="proj_status">Project Status</label>
+          <input
+            type="text"
+            id="projectStatus"
+            name="projectStatus"
+            onChange={formik.handleChange}
+            value={formik.values.projectStatus}
+          />
+          <label for="customer_id">Customer ID</label>
+          <input
+            type="text"
+            id="customerID"
+            name="customerID"
+            onChange={formik.handleChange}
+            value={formik.values.customerID}
+          />
+
+          <div class="flex justify-between mt-6">
+            <button class="btn btn-green">Add</button>
+            <button class="btn btn-gray" onClick={resetForm}>
+              Reset
+            </button>
+          </div>
+        </form>
+      </div>
+
+      <div class="h-16" />
+
+      <div id="Update Project Form" class="p-8 bg-gray-300">
         <form
           class="flex flex-col"
           onSubmit={(event) => {
@@ -58,18 +173,54 @@ export default function DeveloperPage() {
             Update a Project
           </legend>
 
-          <label for="proj_id">Project ID</label>
-          <input type="text" id="proj_id" name="proj_id" />
-          <label for="title">Project Title</label>
-          <input type="text" id="title" name="title" />
+          <label for="projectID">Project ID</label>
+          <input
+            type="text"
+            id="projectID"
+            name="projectID"
+            onChange={formik.handleChange}
+            value={formik.values.projectID}
+          />
+          <label for="projectTitle">Project Title</label>
+          <input
+            type="text"
+            id="projectTitle"
+            name="projectTitle"
+            onChange={formik.handleChange}
+            value={formik.values.projectTitle}
+          />
           <label for="description">Description</label>
-          <input type="text" id="description" name="description" />
+          <input
+            type="text"
+            id="description"
+            name="description"
+            onChange={formik.handleChange}
+            value={formik.values.description}
+          />
           <label for="delivery_date">Delivery Date</label>
-          <input type="date" id="delivery_date" name="delivery_date" />
+          <input
+            type="date"
+            id="deliveryDate"
+            name="deliveryDate"
+            onChange={formik.handleChange}
+            value={formik.values.deliveryDate}
+          />
           <label for="proj_status">Project Status</label>
-          <input type="text" id="proj_status" name="proj_status" />
+          <input
+            type="text"
+            id="projectStatus"
+            name="projectStatus"
+            onChange={formik.handleChange}
+            value={formik.values.projectStatus}
+          />
           <label for="customer_id">Customer ID</label>
-          <input type="text" id="customer_id" name="customer_id" />
+          <input
+            type="text"
+            id="customerID"
+            name="customerID"
+            onChange={formik.handleChange}
+            value={formik.values.customerID}
+          />
 
           <div class="flex justify-between mt-6">
             <button class="btn btn-blue">Update</button>
@@ -80,7 +231,7 @@ export default function DeveloperPage() {
 
       <div class="h-16" />
 
-      <div class="p-8 bg-red-300">
+      <div id="Delete Project Form" class="p-8 bg-red-300">
         <form
           class="flex flex-col"
           onSubmit={(event) => {
