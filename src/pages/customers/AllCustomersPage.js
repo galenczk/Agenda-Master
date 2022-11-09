5376862d287e4fe8c4eab035f1ae5004a63502d4
galenczk <ciszekg@oregonstate.edu>
2022-11-08 20:41:44 -0800

    changed to calling flip2

--- src/pages/customers/AllCustomersPage.js
+++ src/pages/customers/AllCustomersPage.js
@@ -1,10 +1,10 @@
 // Import dependencies
 import React from "react";
 import { useFormik } from "formik";
-import { useState, useEffect } from "react"
+import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 
-import axios from "axios"
+import axios from "axios";
 
 // Import components
 import Navigation from "../../components/Navigation";
@@ -12,80 +12,73 @@ import CustomerTable from "../../components/CustomerTable";
 
 // Page function
 export default function AllCustomersPage() {
+  const [customers, setCustomers] = useState([]);
 
-  const [customers, setCustomers] = useState([])
-  
-  async function loadCustomers(){
-    const response = await axios.get("http://flip3.engr.oregonstate.edu:33525build/customers")
-    const customers = response.data
-    setCustomers(customers)
+  async function loadCustomers() {
+    const response = await axios.get("http://flip2.engr.oregonstate.edu:33525/customers");
+    const customers = response.data;
+    setCustomers(customers);
   }
 
-  useEffect(() =>{
-    loadCustomers()
-  }, [])
+  useEffect(() => {
+    loadCustomers();
+  }, []);
 
   function onView() {
     // Make this developers/:id for dynamic routing
     //navigate("/customer")
   }
 
-  const navigate = useNavigate()
+  const navigate = useNavigate();
 
   // DOM return
   return (
     <>
-      <h1 class="text-3xl p-4">
-        Customers
-      </h1>
+      <h1 class="text-3xl p-4">Customers</h1>
 
       <CustomerTable customers={customers} button={"View"} buttonFunc={onView} />
-      
-<div class="flex-grow"/> 
 
-<div id="add and search" class="flex justify-between">
-  <button class="btn btn-green m-6 place-self-end" onClick={() => {navigate("/add-customer")}}>Add a New Customer</button>
+      <div class="flex-grow" />
 
-  <div  class="p-4 bg-gray-200 w-1/2 m-6">
-        <form class="flex flex-col">
-          <legend class="text-lg font-bold mb-4">
-            Search Customers
-          </legend>
+      <div id="add and search" class="flex justify-between">
+        <button
+          class="btn btn-green m-6 place-self-end"
+          onClick={() => {
+            navigate("/add-customer");
+          }}
+        >
+          Add a New Customer
+        </button>
 
-          <div class="flex">
-            <div class="flex flex-col flex-grow">
-              
-              <label for="title">First Name</label>
-              <input type="text" id="title" name="title" />
-              <label for="title">Last Name</label>
-              <input type="text" id="title" name="title" />
-              
+        <div class="p-4 bg-gray-200 w-1/2 m-6">
+          <form class="flex flex-col">
+            <legend class="text-lg font-bold mb-4">Search Customers</legend>
+
+            <div class="flex">
+              <div class="flex flex-col flex-grow">
+                <label for="title">First Name</label>
+                <input type="text" id="title" name="title" />
+                <label for="title">Last Name</label>
+                <input type="text" id="title" name="title" />
+              </div>
+
+              <div class="w-6" />
+
+              <div class="flex flex-col flex-grow">
+                <label for="title">Company</label>
+                <input type="text" id="title" name="title" />
+                <label for="title">Customer ID</label>
+                <input type="text" id="title" name="title" />
+              </div>
             </div>
 
-            <div class="w-6"/>
-            
-            <div class="flex flex-col flex-grow"> 
-              
-              <label for="title">Company</label>
-              <input type="text" id="title" name="title" />
-              <label for="title">Customer ID</label>
-              <input type="text" id="title" name="title" />
+            <div class="flex justify-between mt-6">
+              <button class="btn-small btn-gray">Reset</button>
+              <button class="btn btn-blue">Search</button>
             </div>
-
-            
-          </div>
-
-          
-
-          <div class="flex justify-between mt-6">
-            
-            <button class="btn-small btn-gray">Reset</button>
-            <button class="btn btn-blue">Search</button>
-          </div>
-        </form>
+          </form>
+        </div>
       </div>
-</div>
-
     </>
   );
 }
