651b4fecccaeb94c765c506b8558fa34d9317834
galenczk <ciszekg@oregonstate.edu>
2022-12-02 14:20:29 -0800

    Added dropdown fields to add forms with FK relationships

--- src/pages/tasks/AddTaskPage.js
+++ src/pages/tasks/AddTaskPage.js
@@ -1,35 +1,74 @@
 // Import dependencies
 import React from "react";
-import { useFormik } from "formik";
+import { Formik, Field, Form } from "formik";
+import { useNavigate } from "react-router-dom";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 // Import components
 
 // Page function
 export default function AddTaskPage() {
- 
+  const [projects, setProjects] = useState([]);
+  const navigate = useNavigate();
 
+  async function loadProjects() {
+    const response = await axios.get("http://flip2.engr.oregonstate.edu:33522/projects");
+    const projects = response.data;
+    console.log(projects);
+    setProjects(projects);
+  }
+
+  useEffect(() => {
+    loadProjects();
+  }, []);
 
   // DOM return
   return (
     <>
       <h1 class="text-3xl p-6 text-center"> Add a Task</h1>
 
-      <div id="Add Project Form" class="p-8 bg-gray-300 mx-auto w-1/2">
-        <form class="flex flex-col">
-          <label for="first_name">Description </label>
-          <input type="text" id="first_name" name="first_name" />
-          <label for="last_name">Due Date</label>
-          <input type="text" id="last_name" name="last_name" />
-          <label for="email">Priority</label>
-          <input type="date" id="email" name="email" />
-          <label for="phone_number">Status</label>
-          <input type="text" id="phone_number" name="phone_number" />
+      <div id="Add Task Form" class="p-8 bg-gray-300 mx-auto w-1/2">
+        <Formik
+          initialValues={{
+            description: "",
+            due_date: "",
+            priority: "",
+            status: "",
+            project_id: "",
+          }}
+        >
+          <Form class="flex flex-col">
+            <label for="description">Description </label>
+            <Field type="text" id="description" name="first_descriptionname" />
+            <label for="due_date">Due Date</label>
+            <Field type="text" id="due_date" name="due_date" />
+            <label for="priority">Priority</label>
+            <Field type="date" id="priority" name="priority" />
+            <label for="status">Status</label>
+            <Field as="select" id="status" name="status" />
+            <label for="project">Project</label>
+            <Field as="select" id="project" name="project">
+              <option value=""> - </option>
+              {projects.map((item, key) => {
+                return (
+                  <option key={key + 1} value={item.project_id}>
+                    {item.title}
+                  </option>
+                );
+              })}
+            </Field>
 
-          <div class="flex justify-between mt-6">
-            <button class="btn-small btn-gray">Reset</button>
-            <button class="btn btn-green">Add</button>
-          </div>
-        </form>
+            <div class="flex justify-between mt-6">
+              <button class="btn-small btn-gray" type="reset">
+                Reset
+              </button>
+              <button class="btn btn-green" type="submit">
+                Add
+              </button>
+            </div>
+          </Form>
+        </Formik>
       </div>
     </>
   );
--- src/pages/tasks/AllTasksPage.js
+++ src/pages/tasks/AllTasksPage.js
@@ -1,6 +1,6 @@
 // Import dependencies
 import React from "react";
-import { useFormik } from "formik";
+import { Formik, Form, Field } from "formik";
 
 // Import components
 import Navigation from "../../components/Navigation"
@@ -30,22 +30,65 @@ export default function AllTasksPage() {
   // DOM return
   return (
     <>
-      <h1 class="text-3xl p-4">
-        All Tasks
-      </h1>
-          
-      <TaskTable tasks={tasks} onEdit={onEdit} />
+      <h1 class="text-3xl p-4">All Tasks</h1>
 
-      
+      <TaskTable tasks={tasks} onEdit={onEdit} />
 
       <div class="flex-grow" />
 
-<div class="flex justify-between">
-  <button class="btn btn-green m-6 place-self-end" onClick={() => {navigate("/add-task")}}>Add a New Task</button>
+      <div className="flex justify-between">
+        <div class="flex justify-between">
+          <button
+            class="btn btn-green m-6 place-self-end"
+            onClick={() => {
+              navigate("/add-task");
+            }}
+          >
+            Add a New Task
+          </button>
+        </div>
 
+        <div class="p-4 bg-gray-200 w-1/2 m-6">
+          <Formik
+            initialValues={{
+              ID: "",
+              dueDate: "",
+              priority: "",
+              status: "",
+              project: "",
+            }}
+          >
+            <Form class="flex flex-col">
+              <legend class="text-lg font-bold mb-4">Search Tasks</legend>
 
-</div>
-      
+              <div class="flex">
+                <div class="flex flex-col flex-grow">
+                  <label for="task_ID">Task ID</label>
+                  <Field type="text" id="task_ID" name="task_ID" />
+                  <label for="dueDate">Due Date</label>
+                  <Field type="text" id="dueDate" name="dueDate" />
+                  <label for="priority">Priority</label>
+                  <Field type="text" id="priority" name="priority" />
+                </div>
+
+                <div class="w-6" />
+
+                <div class="flex flex-col flex-grow justify-center">
+                  <label for="status">Status</label>
+                  <Field type="text" id="status" name="status" />
+                  <label for="project">Project</label>
+                  <Field type="text" id="project" name="project" />
+                </div>
+              </div>
+
+              <div class="flex justify-between mt-6">
+                <button class="btn-small btn-gray">Reset</button>
+                <button class="btn btn-blue">Search</button>
+              </div>
+            </Form>
+          </Formik>
+        </div>
+      </div>
     </>
   );
 }
